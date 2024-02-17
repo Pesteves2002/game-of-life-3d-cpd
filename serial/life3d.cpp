54e1332e052100e745f017fe70c1efee5e10bb19
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2024-02-17 17:54:54 +0000

    feat: read input

--- serial/life3d.cpp
+++ serial/life3d.cpp
@@ -1,7 +1,24 @@
 #include <omp.h>
 #include <cstdio>
+#include <cstdlib>
 
-int main () {
+int gen_num;
+int cells_per_side;
+float density;
+int seed;
+
+int main (int argc, char *argv[]) {
+
+ if (argc != 5) {
+    fprintf(stderr, "Usage: life3d <num_generations> <cells_per_side> <density> <seed>\n");
+    return 1;
+}
+
+  gen_num = atoi(argv[1]);
+  cells_per_side = atoi(argv[2]);
+  density = atof(argv[3]);
+  seed = atoi(argv[4]);
+
   double exec_time;
 
   // grid = gen_initial_grid(N, seed, density);
