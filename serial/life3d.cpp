4fc1517d38d23af89f1806ad772d667ce62eb237
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2024-02-17 17:48:59 +0000

    feat: add execution time

--- serial/life3d.cpp
+++ serial/life3d.cpp
@@ -1,5 +1,17 @@
+#include <omp.h>
+#include <cstdio>
+
 int main () {
-  // ...
+  double exec_time;
+
+  // grid = gen_initial_grid(N, seed, density);
+  exec_time = -omp_get_wtime();
+
+  // simulation();
+
+  exec_time += omp_get_wtime();
+  fprintf(stderr, "%.1fs\n", exec_time);
+
   return 0;
 }
 
