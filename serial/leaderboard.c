65de09d6075654064459efd0683381856b3968cc
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2024-02-26 21:05:08 +0000

    feat: improve leaderboard performance

--- serial/leaderboard.c
+++ serial/leaderboard.c
@@ -1,15 +1,15 @@
 #include "leaderboard.h"
 
-long long leaderboard[N_SPECIES * 3] = {0}; // current, max, max gen
+long long leaderboard[(N_SPECIES + 1) * 3] = {0}; // current, max, max gen
 
 void clearLeaderboard() {
-  for (int i = 0; i < N_SPECIES; i++) {
+  for (int i = 0; i < N_SPECIES + 1; i++) {
     leaderboard[i] = 0;
   }
 };
 
 void updateMaxScores(int current_gen) {
-  for (int i = 0; i < N_SPECIES; i++) {
+  for (int i = 1; i < N_SPECIES + 1; i++) {
     if (leaderboard[i] > leaderboard[i + N_SPECIES]) {
       leaderboard[i + N_SPECIES] = leaderboard[i];
       leaderboard[i + N_SPECIES * 2] = current_gen;
@@ -17,16 +17,11 @@ void updateMaxScores(int current_gen) {
   }
 };
 
-void writeToLeaderboard(char new_state) {
-  if (new_state == 0) {
-    return;
-  }
-  leaderboard[new_state - 1]++;
-};
+void writeToLeaderboard(unsigned char new_state) { leaderboard[new_state]++; };
 
 void printLeaderboard() {
-  for (int i = 0; i < N_SPECIES; i++) {
-    fprintf(stdout, "%d %lld %lld\n", i + 1, leaderboard[i + N_SPECIES],
+  for (int i = 1; i < N_SPECIES + 1; i++) {
+    fprintf(stdout, "%d %lld %lld\n", i, leaderboard[i + N_SPECIES],
             leaderboard[i + N_SPECIES * 2]);
   }
 };
