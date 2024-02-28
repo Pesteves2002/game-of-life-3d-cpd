85ed8da21a3e029a0ceaf11df8730af0edb13ef3
Lucas Pinto <github@lucks.pt>
2024-02-28 00:17:50 +0000

    forgot to update .h

--- openmp/simulation.h
+++ openmp/simulation.h
@@ -9,7 +9,7 @@ void initializeAux(Cube *c, int gen_num, int grid_size);
 
 void simulation();
 
-void debugPrintGrid();
+void debugPrintGrid(unsigned char *grid);
 
 void updateGridState();
 
@@ -22,6 +22,9 @@ void writeCellState(int x, int y, int z, int index, unsigned char old_state,
 
 unsigned char calculateNextState(int x, int y, int z,
                                  unsigned char current_state, int index);
+
 unsigned char getMostFrequentValue(int x, int y, int z);
+
+void commitState();
 
 #endif // SIMULATION_H
